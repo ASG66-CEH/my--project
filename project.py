1900c35ca6cb737d23209522cb845ebeed159b98
ASG66-CEH <sinaighodeabhiv@gmail.com>
2026-04-10 11:11:42 +0530

    Create project.py
    added buggy code to review

---
+++ project.py
@@ -0,0 +1,15 @@
+
+def calculate_sum(a, b):
+    return a + b
+
+numbers = [1, 2, 3]
+
+# Bug 1: wrong variable name
+for i in range(len(number)):
+    print(number[i])
+
+# Bug 2: wrong function usage
+print(calculate_sum(10))
+
+# Bug 3: type error
+print("Total: " + 100)
